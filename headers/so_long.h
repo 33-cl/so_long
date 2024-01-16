775a343465d7b94fb2055244b3ec8255bbc5fd61
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2024-01-16 20:01:23 +0100

    Adding headers

---
+++ headers/so_long.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maeferre <maeferre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 19:57:18 by maeferre          #+#    #+#             */
+/*   Updated: 2024/01/16 18:51:36 by maeferre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdlib.h>
+# include <stdbool.h>
+# include <fcntl.h>
+# include "../minilibx-linux/mlx.h"
+# include "get_next_line.h"
+
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	**ft_split(char const *s, char c);
+bool	parsing(char **map);
+
+#endif
